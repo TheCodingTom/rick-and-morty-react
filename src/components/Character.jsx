41bc6093421aa9bca69cc48eed56557626443213
TheCodingTom <dastolto.t@gmail.com>
2025-01-15 11:46:55 +0100

    "styled" modal

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import React from "react";
 import ModalCharacter from "./ModalCharacter";
 
 function Character(props) {
   // information arriving to children from parent component via props
-
-  // const [show, setShow] = useState(false);
-
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
 
   // console.log(props.character);
   return (
@@ -25,30 +19,7 @@ function Character(props) {
           <div className="flip-card-back">
             <h3 className="card-title">{props.character.name}</h3>
             <ModalCharacter character={props.character} key={props.character.id}/>
-            {/* <Button
-              variant="success"
-              style={{ width: "70%" }}
-              onClick={handleShow}
-            >
-              Read more
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>{props.character.name}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <img
-                  src={props.character.image}
-                  alt="image of a Rick & Morty character"
-                />
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal> */}
+            
           </div>
         </div>
       </div>
--- src/components/ModalCharacter.jsx
+++ src/components/ModalCharacter.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 function ModalCharacter(props) {
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
-    
-      const handleClose = () => setShow(false);
-      const handleShow = () => setShow(true);
-
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div>
-        <Button
-              variant="success"
-              style={{ width: "70%" }}
-              onClick={handleShow}
-            >
-              Read more
-            </Button>
+      <Button variant="success" onClick={handleShow}>
+        Read more
+      </Button>
 
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>{props.character.name}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <img
-                  src={props.character.image}
-                  alt="image of a Rick & Morty character"
-                />
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title className="my-modal">{props.character.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="my-modal">
+          <img
+            src={props.character.image}
+            alt="image of a Rick & Morty character"
+          />
+          <p>Status: {props.character.status}</p>
+          <p>Last location: {props.character.location.name}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            className="my-modal"
+            variant="secondary"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default ModalCharacter
+export default ModalCharacter;
